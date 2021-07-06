8cdff01dd538c46147e92d4fa391add2d655fd0e
Ole <ole@benstem.de>
2021-07-06 18:56:09 +0200

    Build Location class and worked on getters for the client

--- pykanka/__init__.py
+++ pykanka/__init__.py
@@ -1,17 +1,30 @@
 import requests
-import requests_cache
-import json
 import typing
 import tenacity
+from time import time
 
+from requests_cache import CachedSession
+
+import pykanka.entities
 from pykanka.exceptions import *
 
 
 class KankaClient:
     """Main client for interacting with the Kanka.io API"""
 
-    def __init__(self, token: str, campaign: typing.Union[str, int], **kwargs):
-        # self.cache = requests_cache.install_cache("kanka_cache", backend="sqlite", expire_after=600)
+    def __init__(self, token: str, campaign: typing.Union[str, int], cache: bool = True, cache_name: str = "kanka_cache", cache_duration: int = 600, **kwargs):
+        """Create a client associated with a specific campaign.
+
+        :param token: str
+        :param campaign: Union[str, int]
+        :param cache: bool
+        :param cache_name: str
+        :param cache_duration: int
+        :param kwargs:
+        """
+
+        if cache:
+            self.cache = CachedSession(cache_name, backend="sqlite", expire_after=cache_duration)
 
         self.api_token = token
         self.headers = {
@@ -51,24 +64,93 @@ class KankaClient:
         raise CampaignError(f"No campaign of the name '{name}' found")
 
     @tenacity.retry(retry=tenacity.retry_if_exception_type(ApiThrottlingError), wait=tenacity.wait_fixed(5))
-    def _request(self, method, url, **kwargs):
-        response = requests.request(method=method, url=url, headers=self.headers, **kwargs)
+    def _request(self, method, url, refresh=False, **kwargs):
+        if refresh:
+            with self.cache.cache_disabled():
+                response = self.cache.request(method=method, url=url, headers=self.headers, **kwargs)
+        else:
+            response = self.cache.request(method=method, url=url, headers=self.headers, **kwargs)
+
         if response.status_code == 429:
             print("API request limit reached. Retrying in 5 seconds.")
             raise ApiThrottlingError()
 
         return response
 
-    def get(self, url, **kwargs):
-        return self._request("get", url, **kwargs)
+    def request_get(self, url, refresh=False, **kwargs):
+        """get request with proper headers. usually shouldn't be accessed directly."""
+        return self._request("get", url, refresh=refresh, **kwargs)
 
-    def post(self, url, **kwargs):
+    def request_post(self, url, **kwargs):
+        """post request with proper headers. usually shouldn't be accessed directly."""
         return self._request("post", url, **kwargs)
 
-    def put(self, url, **kwargs):
+    def request_put(self, url, **kwargs):
+        """put request with proper headers. usually shouldn't be accessed directly."""
         return self._request("put", url, **kwargs)
 
-    def patch(self, url, **kwargs):
+    def request_patch(self, url, **kwargs):
+        """patch request with proper headers. usually shouldn't be accessed directly."""
         return self._request("patch", url, **kwargs)
+
+    def request_delete(self, url, **kwargs):
+        """delete request with proper headers. usually shouldn't be accessed directly."""
+        return self._request("delete", url, **kwargs)
+
+    def entity(self, entity_id: int) -> "pykanka.entities.Entity":
+        return pykanka.entities.Entity.from_id(self, entity_id)
+
+    def location(self, location_id: int) -> "pykanka.entities.Location":
+        return pykanka.entities.Location.from_id(self, location_id)
+
+    def get_all_entities(self) -> typing.Dict[typing.Tuple[str, int], "pykanka.entities.Entity"]:
+
+        url = f"{self.campaign_base_url}entities"
+        done = False
+        members = dict()
+
+        while not done:
+            response = self.request_get(url)
+
+            if not response.ok:
+                raise ResponseNotOkError(f"Code {response.status_code}: {response.text}")
+
+            content = response.json()
+
+            if not content["links"]["next"]:
+                done = True
+            else:
+                url = content["links"]["next"]
+
+            for entry in content["data"]:
+                entity = pykanka.entities.Entity.from_json(self, entry)
+                members[(entity.data.name, entity.data.id)] = entity
+
+        return members
+
+    def get_all_locations(self) -> typing.Dict[typing.Tuple[str, int], "pykanka.entities.Location"]:
+
+        url = f"{self.campaign_base_url}locations"
+        done = False
+        members = dict()
+
+        while not done:
+            response = self.request_get(url)
+
+            if not response.ok:
+                raise ResponseNotOkError(f"Code {response.status_code}: {response.text}")
+
+            content = response.json()
+
+            if not content["links"]["next"]:
+                done = True
+            else:
+                url = content["links"]["next"]
+
+            for entry in content["data"]:
+                location = pykanka.entities.Location.from_json(self, entry)
+                members[(location.data.name, location.data.id)] = location
+
+        return members
 
 
--- pykanka/entities/__init__.py
+++ pykanka/entities/__init__.py
@@ -1,12 +1,31 @@
 import json
 import typing
 
-from pykanka import KankaClient
+from PIL import Image
+from io import BytesIO
+
+import pykanka
 from pykanka.exceptions import *
 
 
 class Entity:
     """Base class from which specific entity classes like locations and characters are inherited. Should usually not be interacted with directly."""
+
+    type_dictionary = dict(location=pykanka.entities.Location,
+                           character=pykanka.entities.Character,
+                           family=pykanka.entities.Family,
+                           organisation=pykanka.entities.Organization,
+                           timeline=pykanka.entities.Timeline,
+                           race=pykanka.entities.Race,
+                           note=pykanka.entities.Note,
+                           map=pykanka.entities.Map,
+                           tag=pykanka.entities.Tag,
+                           quest=pykanka.entities.Quest,
+                           journal=pykanka.entities.Journal,
+                           item=pykanka.entities.Item,
+                           event=pykanka.entities.Event,
+                           ability=pykanka.entities.Ability
+                           )
 
     class EntityData:
         def __init__(self, val: dict = None):
@@ -37,7 +56,7 @@ class Entity:
                     else:
                         raise WrongParametersPassedToEntity(f"{key} has been passed to Entity class, but is not a valid parameter")
 
-    def __init__(self, client: "KankaClient", child: "EntityType" = None):
+    def __init__(self, client: "KankaClient"):
         """
         Generates empty Entity. Consider using Entity.from_id() or Entity.from_json() instead.
 
@@ -46,22 +65,24 @@ class Entity:
 
         self.client = client
         self.data = self.EntityData()
+        self.entity_id = None
 
     @classmethod
-    def from_id(cls, client: "KankaClient", entity_id: int) -> "Entity":
+    def from_id(cls, client: "pykanka.KankaClient", entity_id: int) -> "Entity":
         obj = Entity(client)
 
-        response = client.get(f"{client.campaign_base_url}entities/{entity_id}")
+        response = client.request_get(f"{client.campaign_base_url}entities/{entity_id}")
 
         if not response.ok:
             raise ResponseNotOkError(f"Response not OK, code {response.status_code}:\n{response.text}")
 
         obj.data = obj.EntityData(response.json())
+        obj.entity_id = obj.data.id
 
         return obj
 
     @classmethod
-    def from_json(cls, client: "KankaClient", content: typing.Union[str, dict]) -> "Entity":
+    def from_json(cls, client: "pykanka.KankaClient", content: typing.Union[str, dict]) -> "Entity":
 
         if type(content) == str:
             content = json.loads(content)
@@ -69,12 +90,19 @@ class Entity:
         obj = Entity(client)
 
         obj.data = cls.EntityData(val=content)
+        obj.entity_id = obj.data.id
 
         return obj
 
+    def _request_data(self):
+        response = self.client.request_get(f"{self.client.campaign_base_url}entities/{self.entity_id}")
 
-class EntityType:
-    class GenericData:
+        if not response.ok:
+            raise ResponseNotOkError(f"Response not OK, code {response.status_code}:\n{response.text}")
+
+
+class GenericChildType:
+    class GenericChildData:
         def __init__(self, val: dict = None):
             self.id = None
             self.type = None
@@ -112,17 +140,21 @@ class EntityType:
                     else:
                         raise WrongParametersPassedToEntity(f"{key} has been passed to child class, but is not a valid parameter")
 
-
-    def __init__(self, client: "KankaClient", parent: "Entity" = None):
+    def __init__(self, client: "pykanka.KankaClient", parent: "Entity" = None):
 
         self.client = client
         self.parent = parent
 
+    def refresh(self):
+        pass
 
-class Location(EntityType):
+    def _request_values(self):
+        pass
+
+class Location(GenericChildType):
     """A class representing a location child contained within an Entity."""
 
-    class LocationData(EntityType.GenericData):
+    class LocationData(GenericChildType.GenericChildData):
         def __init__(self, val: dict = None):
             self.parent_location_id = None
             self.is_map_private = None
@@ -130,8 +162,131 @@ class Location(EntityType):
 
             super().__init__(val=val)
 
-    def __init__(self, client: "KankaClient", entity: "Entity"):
-        super().__init__(client)
+    def __init__(self, client: "pykanka.KankaClient", parent: "Entity" = None):
+        """
+        Creates an empty Location. Consider using Location.from_id() or Location.from_json() instead.
+
+        :param client: KankaClient
+        :param parent: Entity
+        """
+        super().__init__(client, parent=parent)
+
+        self.data = self.LocationData()
+        self.location_id = None
+
+        self.base_url = f"{self.client.campaign_base_url}locations/"
+
+    @classmethod
+    def from_id(cls, client: "pykanka.KankaClient", location_id: int) -> "Location":
+        obj = Location(client)
+
+        response = client.request_get(f"{client.campaign_base_url}entities/{location_id}")
+
+        if not response.ok:
+            raise ResponseNotOkError(f"Response not OK, code {response.status_code}:\n{response.text}")
+
+        obj.data = obj.L(response.json())
+        obj.entity_id = obj.data.id
+
+        return obj
+
+    @classmethod
+    def from_json(cls, client: "pykanka.KankaClient", content: typing.Union[str, dict]) -> "Location":
+
+        if type(content) == str:
+            content = json.loads(content)
+
+        obj = Location(client)
+
+        obj.data = cls.LocationData(val=content)
+        obj.location_id = obj.data.id
+
+        return obj
+
+    def _get_post_values(self):
+        possible_keys = ["name", "type", "parent_location_id", "tags", "is_private", "image", "map", "is_map_private"]
+
+        values = dict()
+
+        for key in possible_keys:
+            if self.location.__dict__[key] is not None:
+                values[key] = self.location.__dict__[key]
+
+        if "image" in values.keys():
+            values["image_url"] = values["image"]
+            values.pop("image")
+        if "map" in values.keys():
+            values["map_url"] = values["map"]
+            values.pop("map")
+
+        return values
+
+    def post(self, json_data: str = None, name: str = "", **kwargs):
+        """
+        Posts Location to Campaign. Possible parameters are outlined in the documentation, here:
+        https://kanka.io/en/docs/1.0/locations#create-location
+        Parameters are taken from existing object, a json string or keywords.
+        Here, keywords override json override existing.
+
+        Currently, the kanka API does not support the parameters 'image' and 'map'.
+
+        :param json_data: str
+        :param name: str
+        :return: requests.response
+        """
+
+        payload, files = self._prepare_post(json_data, name=name, **kwargs)
+
+        return self.client.request_post(f"{self.base_url}", json=payload)
+
+    def patch(self, json_data: str = None, name: str = "", **kwargs):
+        """
+        Updates Location to Campaign. Possible parameters are outlined in the documentation, here:
+        https://kanka.io/en/docs/1.0/locations#create-location
+        Parameters are taken from existing object, a json string or keywords.
+        Here, keywords override json override existing.
+
+        Currently, the kanka API does not support the parameters 'image' and 'map'.
+
+        :param json_data: str
+        :param name: str
+        :return: requests.response
+        """
+
+        payload, files = self._prepare_post(json_data, name=name, **kwargs)
+
+        return self.client.request_patch(f"{self.base_url}{self.location_id}", json=payload)
+
+    def delete(self):
+        return self.client.request_delete(f"{self.base_url}{self.location_id}")
+
+    def _prepare_post(self, json_data: str, **kwargs):  # implement support for image files (keys: image and map) when the API allows it
+        if json_data:
+            values = json.loads(json_data)
+            values.update(kwargs)
+        else:
+            values = kwargs
+
+        # The API doesn't support file uploads as of now, placeholder to when it will become relevant.
+
+        files = dict()
+
+        if "image" in values:
+            files["image"] = open(values["image"], "rb")
+            values.pop("image")
+            print("API doesn't support direct file uploads yet, use url instead")
+        if "map" in values:
+            files["map"] = open(values["map"], "rb")
+            values.pop("map")
+            print("API doesn't support direct file uploads yet, use url instead")
+
+        existing_values = self._get_post_values()
+        existing_values.update(values)
+
+        if "name" not in existing_values.keys():
+            raise ValueError("'name' is a required field, but is missing")
+
+        return existing_values, files
 
 
 class Character:
