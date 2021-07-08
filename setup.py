7680015b7c4e3f42acbd39a65cf6391ed1f4879d
Ole <ole@benstem.de>
2021-07-08 23:43:12 +0200

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open ("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="pykanka",
+    version="0.1.0",
+    description="A wrapper for the kanka.io API, currently providing basic get/patch/post functionality for most entity classes",
+    author="Spectre",
+    packages=["pykanka"],
+    install_requires=["requests", "tenacity", "json", "typing"]
+)
