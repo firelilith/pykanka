8300b0b886a8fdcb308cadfd851125170694a6a2
asc <a@correa.co>
2021-10-09 14:29:12 -0500

    add kanka credentials sample file

---
+++ tests/kanka_credentials_sample.py
@@ -0,0 +1,5 @@
+# Replace the values below with your kanka token and a campaign id you have write access to
+# then remove "_sample" from file name so that the file name is kanka_credentials.py
+# https://kanka.io/en-US/docs/1.0/setup
+KANKA_TOKEN = "{TOKEN}"
+CAMPAIGN_ID = 1
