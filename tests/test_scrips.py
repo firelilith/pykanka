3eab5de3312b63a447b2b81ee6acee76ded31eac
Spectre <ole@benstem.de>
2022-12-14 16:48:13 +0100

    tests

--- tests/test_scrips.py
+++ tests/test_scrips.py
@@ -7,3 +7,6 @@ logging.getLogger(__name__)
 with open("secrets", "r") as f:
     token = f.readline()
 
+client = CampaignClient(campaign_id=86924, token=token)
+client.create_entity("character", name="Frederik", image_url="https://picsum.photos/512")
+
